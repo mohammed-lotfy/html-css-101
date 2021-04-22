90c576cc2d52b43980c745180927ed6ec652d666
Mohammed Lotfy <m.lotfy.personal@gmail.com>
2021-04-23 00:59:40 +0200

    Some changes!

---
+++ app.js
@@ -0,0 +1,20 @@
+
+function calc(fn, x, y) {
+	return fn(x, y);
+}
+
+
+function add(x, y) {
+	 return x + y;
+}
+
+
+function multiply(x, y) {
+	return x * y;
+}
+
+
+let x = 7, y = 4;
+let result = calc(add, x, y);
+let result = calc(multiply, x, y);
+console.log(result);
